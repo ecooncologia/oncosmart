b66234994bb3ca19b077f97ed679a186a8cbffcd
Felipe Leite <felipe.leite.eco@ecooncologia.com.br>
2026-03-18 17:29:30 -0300

    Retornando Global e Styles

--- global.js
+++ global.js
@@ -1,14 +1,3 @@
-// ==========================================
-// MÁGICA DA RESPONSIVIDADE (MOBILE)
-// Injeta a tag viewport em todas as páginas automaticamente
-// ==========================================
-if (!document.querySelector('meta[name="viewport"]')) {
-    let meta = document.createElement('meta');
-    meta.name = "viewport";
-    meta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no";
-    document.head.appendChild(meta);
-}
-
 let API_BASE_URL = "https://api.ecooncologia.com.br:3000";
 
 if (window.location.href.includes('/teste/')) {
